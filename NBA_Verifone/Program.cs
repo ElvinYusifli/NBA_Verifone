63bd7e175db74230843496a5e4a9f5f94200e37b
Elvin Yusifli <elvin.yusifli@mktcotton.com>
2020-05-08 14:23:48 +0400

    Intial Commit

--- NBA_Verifone/Program.cs
+++ NBA_Verifone/Program.cs
@@ -16,7 +16,8 @@ namespace test
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Main());
+            Application.Run(new Main()); 
+
 
         }
     }
